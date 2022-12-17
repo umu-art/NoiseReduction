999123a1599cf4282ae3c605a87fa3bd6b1930fa
V0S7ER <k1ng.brown@yandex.ru>
2022-12-17 12:01:21 +0400

    Add feed_forward, conformer_block, res_con

---
+++ layers/ConformerBlock.py
@@ -0,0 +1,24 @@
+from torch import nn
+
+from layers.FeedForwardModule import FeedForwardModule
+
+
+class ConformerBlock(nn.Module):
+    def __init__(self, size: int):
+        super().__init__()
+        self.feed_forward_first = FeedForwardModule(size)
+        # self.multi_head = ... # TODO: добавить MultiHeadAttentionModule Саши
+        # self.conv = ConvolutionModule() # TODO: добавить ConvolutionModule
+        self.feed_forward_second = FeedForwardModule(size)
+        self.layer_norm = nn.LayerNorm(size)
+
+    def forward(self, x):
+        x_after_first_feed_forward = self.feed_forward_first(x)
+        x_before_multi_head = x * x_after_first_feed_forward / 2
+        x_after_multi_head = self.multi_head(x_before_multi_head)
+        x_before_conv = x_before_multi_head + x_after_multi_head
+        x_after_conv = self.conv(x_before_conv)
+        x_before_second_feed_forward = x_before_conv + x_after_conv
+        x_after_second_feed_forward = self.feed_forward_second(x_before_second_feed_forward)
+        x_before_norm = x_before_second_feed_forward + x_after_second_feed_forward / 2
+        return self.layer_norm(x_before_norm)
