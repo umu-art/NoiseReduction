bba1d4feca0fb06a1f9b05cb7558f467949fb22e
Vladimir <kazeninvladimir@gmail.com>
2022-12-17 23:44:20 +0300

    Prepare to collab

--- main.py
+++ main.py
@@ -15,9 +15,9 @@ from Conformer import Conformer
 
 dataset = MixtureDataset(16000, (0, 10), 1500)
 dataset.clean_speech_data_paths = dataset.clean_speech_data_paths
-dataset.noise_paths = dataset.noise_paths[:20]
+dataset.noise_paths = dataset.noise_paths
 
-data_loader_train = DataLoader(dataset, batch_size=150, shuffle=False)
+data_loader_train = DataLoader(dataset, batch_size=15, shuffle=False)
 data_loader_valid = DataLoader(dataset, batch_size=10, shuffle=False)
 
 data_loader_train = CudaDataLoader(data_loader_train)
@@ -28,7 +28,7 @@ loss_fn = nn.L1Loss()
 model = Conformer(n_fft, hop_length, win_length, window, size, conf_blocks_num, conv_kernel_size)
 to_cuda(model)
 
-optimizer = torch.optim.Adam(model.parameters(), betas=(0.9, 0.999), lr=1e-3)
+optimizer = torch.optim.Adam(model.parameters(), betas=(0.9, 0.999), lr=1e-4)
 
 train(model, optimizer, loss_fn, data_loader_train, data_loader_valid, 15*4, '/out.tar')
 
