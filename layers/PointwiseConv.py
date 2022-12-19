725add55ae5d714200a2565b99e1056aa202ae91
LobanoffMaks <mlpotato@yandex.ru>
2022-12-19 13:44:55 +0300

    add docs for PointwiseConv

--- layers/PointwiseConv.py
+++ layers/PointwiseConv.py
@@ -2,6 +2,18 @@ from torch import nn
 
 
 class PointwiseConv(nn.Module):
+    """
+        PointwiseConv - свертка, которая использует 1x1.
+        Аргументы:
+            channels_in (int) - кол-во входных каналов
+            channels_out (int) - кол-во выходных каналов
+            bias (bool) - использовать/не использовать сдвиг
+        Вход:
+            x (tensor) - тензор, содержащий входной вектор
+        Выход:
+            **output** (tensor) -  тензор после применения свертки
+    """
+
     def __init__(self, channels_in: int, channels_out: int, bias=True):
         super().__init__()
 
