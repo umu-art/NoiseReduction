fb8c720c032875be901d97f9c62351f2c99c800f
dimamkr <120774043+dimamkr@users.noreply.github.com>
2022-12-19 20:37:51 +0300

    Update GLU.py

--- activators/GLU.py
+++ activators/GLU.py
@@ -4,9 +4,18 @@ from torch import nn
 
 class GLU(nn.Module):
     def __init__(self, dim):
+        """
+        arguments:
+        dim(int) the dimension on which to split the input
+        """
         super().__init__()
         self.dim = dim
 
     def forward(self, x: torch.tensor):
+        """
+        arguments:
+        x(torch.tensor) input tensor
+        returns: a ⊗ σ(b) where a is the first half of the input matrices and b is the second half
+        """
         a, b = torch.chunk(x, 2, self.dim)
         return a * torch.sigmoid(b)
