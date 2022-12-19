f1343154ad004d791a6251472614828f37c8efb2
LobanoffMaks <mlpotato@yandex.ru>
2022-12-19 14:45:05 +0300

    add docs for DepthwiseConv

--- layers/DepthwiseConv.py
+++ layers/DepthwiseConv.py
@@ -2,6 +2,18 @@ from torch import nn
 
 
 class DepthwiseConv(nn.Module):
+    """
+            DepthWiseConv - свертка.
+            Аргументы:
+                channels_in (int) - кол-во входных каналов
+                channels_out (int) - кол-во выходных каналов
+                kernel_size (int) - размер свертки
+                bias (bool) - использовать/не использовать сдвиг
+            Вход:
+                x (tensor) - тензор, содержащий входной вектор
+            Выход:
+                **output** (tensor) -  тензор после применения свертки
+    """
     def __init__(self, channels_in: int, channels_out: int, kernel_size: int, bias=True):
         super().__init__()
         assert channels_out % channels_in == 0, 'channels_in must be a divisor of channels_out'
