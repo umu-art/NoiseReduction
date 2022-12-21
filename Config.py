820166c62ff1c957b3c0413a42c30a734c6b05b1
Vladimir Kazenin <64376214+Vladimir-art40@users.noreply.github.com>
2022-12-21 23:23:37 +0300

    Update Config.py

--- Config.py
+++ Config.py
@@ -3,7 +3,7 @@
 part_frames = 16_000 * 3
 
 # Параметры Dataset
-prefix_root = 'E:/'  # начало ссылок на входные данные
+prefix_root = '/home/jupyter/mnt/datasets/data/'
 clean_speech_data_root = f'{prefix_root}LibriSpeech/train-clean-360'  # местоположение директории с чистыми записями для обучения
 noise_root = f'{prefix_root}musan/noise'  # местоположение директории с шумами для обучения
 noise_eval_root = f'{prefix_root}DEMAND'  # местоположение директории с шумами для теста
@@ -11,21 +11,17 @@ clean_pattern = f'{clean_speech_data_root}/**/*.flac'  # паттерн назв
 noise_pattern = f'{noise_root}/**/*.wav'  # паттерн названия файла с шумом для обучения
 noise_eval_pattern = f'{noise_eval_root}/**/*.wav'  # паттерн названия файла с шумом для теста
 cache_folder = 'cache/'  # путь до директории с кешем
-snr_range = (2, 5)  # разброс snr для обучения [left..right]
-chance_same_gender = 0.2
+snr_range = (-2.5, 7.5)  # разброс snr для обучения [left..right]
+chance_same_gender = 0.8
 
 # Параметры модели
-n_fft = 1024  # размер STFT свертки
-hop_length = n_fft // 4  # параметр для STFT
-win_length = n_fft  # размер окна STFT
-window = 'hann_window'  # тип окна STFT (еще есть 'hamm_window')
-size = n_fft // 2  # размер после линейного преобразования в Conformer
-conf_blocks_num = 12  # Количество ConformerBlock'ов в Conformer'е
+size = 256  # размер после линейного преобразования в Conformer
+conf_blocks_num = 6  # Количество ConformerBlock'ов в Conformer'е
 conv_kernel_size = 31  # kernel_size для ConvModule
-w_len = 40
+w_len = 160
 
 # Параметры обучения
-batch_size = 96  # размер выборки для одной итерации по датасету
+batch_size = 32  # размер выборки для одной итерации по датасету
 iters_per_epoch = 1024  # количество итераций на эпоху обучения
 epochs = 100  # количество эпох обучения
 clip_val = 5  # параметр для изменения learning_rate
