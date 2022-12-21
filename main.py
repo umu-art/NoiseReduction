664c12a5aa7e601d114850ab9e15279cad6a39c3
Vladimir <kazeninvladimir@gmail.com>
2022-12-21 22:01:35 +0300

    bot-start

--- main.py
+++ main.py
@@ -1,43 +1,16 @@
-from pathlib import Path
-
-import librosa
-import torch
-
 import Token
-from AudioMetods import save_audio, read_audio
-from model import Config
-from model.Conformer import Conformer
+from AudioMetods import save_audio
 from model.NR_Model import NRModel
 import telebot
 
-model = Conformer(Config.n_fft, Config.hop_length, Config.win_length, Config.window,
-                          Config.size, Config.conf_blocks_num, Config.conv_kernel_size)
-
-snap = torch.load('model/model.tar', map_location='cpu')
-model_state_dict = snap['model']
-model.load_state_dict(model_state_dict)
-model.eval()
-
-mix, sr, d = read_audio('cache/dd.wav')
-mix = mix[:, 0]
-mix = librosa.resample(mix, orig_sr=sr, target_sr=16_000)
-audio = torch.from_numpy(mix)[None]
-ans = model(audio)[0]
-save_audio('out.wav', ans.detach().numpy())
-
-# bot = telebot.TeleBot(Token.token)
-# neiro = NRModel()
-#
-# wave = neiro('cache/dd.wav')
-# save_audio('cache/clean.wav', wave)
+bot = telebot.TeleBot(Token.token)
+neiro = NRModel()
 
 
 def work(abstract, chat_id):
     file_info = bot.get_file(abstract.file_id)
-    file_type = abstract.mime_type[6:]
-
-    if file_type == 'ogg':
-        file_type = 'wav'
+    # file_type = abstract.mime_type[6:]
+    file_type = 'wav'
 
     downloaded_file = bot.download_file(file_info.file_path)
 
