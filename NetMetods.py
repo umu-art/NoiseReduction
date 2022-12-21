8de27b86553e6d71ab5bc54d5d180c3e03b5aef8
Vladimir Kazenin <64376214+Vladimir-art40@users.noreply.github.com>
2022-12-21 23:25:49 +0300

    Update NetMetods.py

--- NetMetods.py
+++ NetMetods.py
@@ -29,9 +29,6 @@ def train_epoch(model: nn.Module, optimizer: optim, scheduler: optim.lr_schedule
             **output** (dict) - словарь со данными про SNR и ф-ию потерь
     """
     model.train()
-    train_snr = 0
-    train_inp_snr = 0
-    train_snr_i = 0
     train_loss = 0
     for mixture, clean in tqdm(data_loader):
         wave = model(mixture)
@@ -43,29 +40,15 @@ def train_epoch(model: nn.Module, optimizer: optim, scheduler: optim.lr_schedule
         Logger.write_lr(scheduler.get_last_lr(), point)
         scheduler.step()
         optimizer.zero_grad()
-        cur_snr = calc_snr(clean.detach().cpu().numpy(),
-                           wave.detach().cpu().numpy() - clean.detach().cpu().numpy()).mean()
-        inp_snr = calc_snr(clean.detach().cpu().numpy(),
-                           mixture.detach().cpu().numpy() - clean.detach().cpu().numpy()).mean()
-        train_snr += cur_snr
-        train_inp_snr += inp_snr
         train_loss += loss.item()
-        train_snr_i += (cur_snr - inp_snr)
 
-        Logger.write_point('train', point, cur_snr, inp_snr, loss.item())
+        Logger.write_point('train', point, loss.item())
         point += 1
     n = len(data_loader)
-    train_snr /= n
     train_loss /= n
-    train_inp_snr /= n
-    train_snr_i /= n
 
-    Logger.write_epoch_point('train_epoch', gl_point, train_snr, train_inp_snr, train_snr_i, train_loss)
-    return {"train_loss": train_loss,
-            "train_snr": train_snr,
-            "train_inp_snr": train_inp_snr,
-            "train_snr_i": train_snr_i
-            }
+    Logger.write_epoch_point('train_epoch', gl_point, train_loss)
+    return {"train_loss": train_loss}
 
 
 @torch.no_grad()
@@ -82,34 +65,20 @@ def val_epoch(model, data_loader, loss_fn, point: int, gl_point: int):
             **output** (dict) - словарь со данными про SNR и ф-ию потерь
     """
     model.eval()
-    val_snr = 0
-    val_inp_snr = 0
     val_loss = 0
-    val_snr_i = 0
     for mixture, clean in data_loader:
         wave = model(mixture)
         loss = loss_fn(wave, clean)
-        cur_snr = calc_snr(clean.detach().cpu().numpy(),
-                           wave.detach().cpu().numpy() - clean.detach().cpu().numpy()).mean()
-        inp_snr = calc_snr(clean.detach().cpu().numpy(),
-                           mixture.detach().cpu().numpy() - clean.detach().cpu().numpy()).mean()
-        val_snr += cur_snr
-        val_inp_snr += inp_snr
-        val_loss += loss.item()
-        val_snr_i += (cur_snr - inp_snr)
 
-        Logger.write_point('eval', point, cur_snr, inp_snr, loss.item())
+        val_loss += loss.item()
+
+        Logger.write_point('eval', point, loss.item())
         point += 1
     n = len(data_loader)
-    val_snr /= n
+    
     val_loss /= n
-    val_inp_snr /= n
-    val_snr_i /= n
-    Logger.write_epoch_point('eval_epoch', gl_point, val_snr, val_inp_snr, val_snr_i, val_loss)
-    return {"val_loss": val_loss,
-            "val_snr": val_snr,
-            "val_inp_snr": val_inp_snr,
-            "val_snr_i": val_snr_i}
+    Logger.write_epoch_point('eval_epoch', gl_point, val_loss)
+    return {"val_loss": val_loss}
 
 
 def train(model, optimizer, scheduler, loss_fn, data_loader_train, data_loader_val, dataset_valid, epochs, save_path,
@@ -132,13 +101,7 @@ def train(model, optimizer, scheduler, loss_fn, data_loader_train, data_loader_v
     # save_path .tar
     logs = {
         "train_loss": [],
-        "train_snr": [],
-        "train_inp_snr": [],
-        "train_snr_i": [],
-        "val_loss": [],
-        "val_snr": [],
-        "val_inp_snr": [],
-        "val_snr_i": [],
+        "val_loss": []
     }
 
     print('Training...')
@@ -150,14 +113,8 @@ def train(model, optimizer, scheduler, loss_fn, data_loader_train, data_loader_v
             test(model, dataset_valid, 'epoch_' + str(epoch))
 
         logs["train_loss"].append(cur_train["train_loss"])
-        logs["train_snr"].append(cur_train["train_snr"])
-        logs["train_inp_snr"].append(cur_train["train_inp_snr"])
-        logs["train_snr_i"].append(cur_train["train_snr_i"])
-
         logs["val_loss"].append(cur_val["val_loss"])
-        logs["val_snr"].append(cur_val["val_snr"])
-        logs["val_inp_snr"].append(cur_val["val_inp_snr"])
-        logs["val_snr_i"].append(cur_val["val_snr_i"])
+        
         snapshot = {
             "model": model.state_dict(),
             "optimizer": optimizer.state_dict(),
@@ -169,24 +126,25 @@ def train(model, optimizer, scheduler, loss_fn, data_loader_train, data_loader_v
 
 
 def test(model, dataset, i: str):
-    f = dataset.clean_dataset.audio_paths[randint(0, len(dataset.clean_dataset.audio_paths) - 1)]
-    audio = read_audio(f)[0]
-    # ashow(audio)
+    pass
+#     f = dataset.clean_dataset.audio_paths[randint(0, len(dataset.clean_dataset.audio_paths) - 1)]
+#     audio = read_audio(f)[0]
+#     # ashow(audio)
 
-    f = dataset.noise_dataset.audio_paths[randint(0, len(dataset.noise_dataset.audio_paths) - 1)]
-    noise = read_audio(f)[0]
-    while len(noise) < len(audio):
-        noise = np.concatenate((noise, noise))
-    noise = noise[:len(audio)]
+#     f = dataset.noise_dataset.audio_paths[randint(0, len(dataset.noise_dataset.audio_paths) - 1)]
+#     noise = read_audio(f)[0]
+#     while len(noise) < len(audio):
+#         noise = np.concatenate((noise, noise))
+#     noise = noise[:len(audio)]
 
-    mixture = audio + calc_coefficient(audio, noise, 2) * noise
-    Logger.save_audio(torch.from_numpy(mixture), str(i) + '_mix')
-    # ashow(mixture)
+#     mixture = audio + calc_coefficient(audio, noise, 2) * noise
+#     Logger.save_audio(torch.from_numpy(mixture), str(i) + '_mix')
+#     # ashow(mixture)
 
-    mixture = torch.from_numpy(mixture)
-    mixture = to_cuda(mixture)
+#     mixture = torch.from_numpy(mixture)
+#     mixture = to_cuda(mixture)
 
-    model.eval()
-    wave = model(mixture[None])[0]
-    Logger.save_audio(wave.cpu().detach(), str(i) + '_wave')
+#     model.eval()
+#     wave = model(mixture[None])[0]
+#     Logger.save_audio(wave.cpu().detach(), str(i) + '_wave')
     # ashow(wave.cpu().detach().numpy())
